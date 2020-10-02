a718877e60a7b25e24c2f8f7c38a6f014190c76d
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:35:26 +0800

    Create UploadProfilePhoto.js

---
+++ 200914MtaHtmlJS/Scripts/UploadProfilePhoto.js
@@ -0,0 +1,19 @@
+﻿/* upload photo via Registration.html */
+function uploadProfilePhoto() {
+    var preview = document.getElementById('ProfilePhoto');
+    var file = document.getElementById("SelectPhoto").files[0];
+    var reader = new FileReader(); //instantiation
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+
+    reader.onloadend = function () {
+        preview.src = reader.result;
+    }
+    reader.onerror = function () {
+        alert("Something went wrong upon image upload...");
+    }
+}
