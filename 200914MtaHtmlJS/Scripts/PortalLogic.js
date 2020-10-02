be70454ecba58f9875df9e54176e71fc5df2d1cd
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:35:34 +0800

    Create PortalLogic.js

---
+++ 200914MtaHtmlJS/Scripts/PortalLogic.js
@@ -0,0 +1,39 @@
+﻿"use strict"    /* can not use undeclared variables. */
+
+/* Employee No. random generate */
+function generateEmpNo() {
+    var vaempId = 0;
+    var vaempNoPrefix = "";
+    var vaempType = document.getElementById("EmpTypeInput").value;
+    switch (vaempType) {
+        case "Regular":
+            vaempNoPrefix = "R";
+            break;
+        case "Temporary":
+            vaempNoPrefix = "T";
+            break;
+        default:
+            vaempNoPrefix = "C";
+    }
+    vaempId = vaempNoPrefix + Math.round(Math.random() * 100 + 1000); //concatenation
+    document.getElementById("EmpNoInput").value = vaempId;
+}
+
+/* validate form from registration.html */
+function validateForm() {
+    var pinInput = $("#EmpPinInput").val();
+    var confPinInput = $("#ConfirmPinInput").val();
+    if (pinInput != confPinInput) {
+        //var confPinError = document.getElementById("empCPinInput");
+        // confPinError.setCustomValidity("PIN inputs don't match...");
+        document.getElementById("ConfirmPinInput").style.color = "red";
+        alert("PIN inputs don't match...");
+        return false;
+    }
+    return true;
+}
+
+/* OffLine, @index,html <body onoffline="hideWhenOffline();"> */
+function hideWhenOffline() {
+    document.getElementById("multimedia").style.display = "none";
+}
