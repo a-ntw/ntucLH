7106aa035debd85c0322fe2b4b519ad48ac03b3b
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:34:46 +0800

    Create LocationService.js

---
+++ 200914MtaHtmlJS/Scripts/LocationService.js
@@ -0,0 +1,27 @@
+﻿/* Map location, at Index.html */
+function renderMap(long, lat) {
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([long, lat]),
+            zoom: 4
+        })
+    });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    renderMap(position.coords.longitude, position.coords.latitude);
+}
