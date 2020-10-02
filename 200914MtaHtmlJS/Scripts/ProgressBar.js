2b237dc7943984739d18a79623fcc077add301c4
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:35:24 +0800

    Create ProgressBar.js

---
+++ 200914MtaHtmlJS/Scripts/ProgressBar.js
@@ -0,0 +1,12 @@
+﻿/* display animated bar at top of Registration  */
+function displayProgress(progress) {
+    var c = document.getElementById("inputProgressBar");
+    var ctx = c.getContext("2d");
+    // Create gradient
+    var grd = ctx.createLinearGradient(0, 0, progress, 0);
+    grd.addColorStop(0, "red");
+    grd.addColorStop(1, "white");
+    // Fill with gradient
+    ctx.fillStyle = grd;
+    ctx.fillRect(10, 10, 1000, 80);
+}
