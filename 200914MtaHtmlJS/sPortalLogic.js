d8fc78eab62c9ac0bdfa84a3b942ae45f833039c
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:35:28 +0800

    Create sPortalLogic.js

---
+++ 200914MtaHtmlJS/sPortalLogic.js
@@ -0,0 +1,16 @@
+﻿"use strict"  
+
+function validateForm() {
+    var pinInput = $("#EmpPinInput").val();
+    var confPinInput = $("#ConfirmPinInput").val();
+    if (pinInput != confPinInput) {
+        //var confPinError = document.getElementById("empCPinInput");
+        // confPinError.setCustomValidity("PIN inputs don't match...");
+        document.getElementById("ConfirmPinInput").style.color = "red";
+        alert("PIN inputs don't match...");
+        return false;
+    }
+    return true;
+}
+
+
