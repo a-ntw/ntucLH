23fbad6b8947531014f210c9db9b25defddf7b5b
AlvinTWNg <alvintwng@gmail.com>
2020-10-02 16:34:09 +0800

    Create AnimationLogic.js

---
+++ 200914MtaHtmlJS/Scripts/AnimationLogic.js
@@ -0,0 +1,12 @@
+﻿/* animate Email click at index.html, script at bottom */
+var inquiryElement = document.getElementById("emailSupport");
+inquiryElement.addEventListener("animationend", onAnimationend, true);
+
+function sendMail() {
+    inquiryElement.className = "clickEmail";
+}
+
+
+function onAnimationend(e) {
+    inquiryElement.className = " ";
+}
