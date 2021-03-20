f5184077a9ad40fd4e2633b3be71c9ab223706ac
AlvinTWNg <alvintwng@gmail.com>
2021-03-21 00:25:46 +0800

    DatePicker, finalDates from sql

--- mJSummativeCapStoneProj/210316-DatePicker/JsController.java
+++ mJSummativeCapStoneProj/210316-DatePicker/JsController.java
@@ -2,6 +2,7 @@ package com.ntuc;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class JsController {
 
+	@Autowired Blockdate d;
+	@Autowired bdatesRepository repo;
+	
 	@GetMapping("/")
 	public String showMain(Model model) {
 
-		List<Blockdate> listBddates = null;
+		List<Blockdate> listBddates = repo.findAll();
 		String[] fdates = calenderBlkDates.fdates(listBddates);
 		
 		model.addAttribute("calenderBlkDates", fdates); // finalDates can only add once
--- mJSummativeCapStoneProj/210316-DatePicker/calenderBlkDates.java
+++ mJSummativeCapStoneProj/210316-DatePicker/calenderBlkDates.java
@@ -1,28 +1,51 @@
 package com.ntuc;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class calenderBlkDates {
-
-	// Array of Blockdates with Localdate startDate and Integer period
-	public static String[] fdates(List<Blockdate> listBddates) {
-		
-		LocalDate abcDate2 = LocalDate.parse("2021-04-20");
-		LocalDate[] listdates1;
-		listdates1 = dateRange(abcDate2, 3);
-		LocalDate abcDate3 = LocalDate.parse("2021-03-22");
-		LocalDate[] listdates3;
-		listdates3 = dateRange(abcDate3, 3);
-
-		LocalDate[] mergedates = mergeDates(listdates1, listdates3 );
-
-		return  finalDates(mergedates);
-	}
 	
+	@Autowired Blockdate d;
+	@Autowired bdatesRepository repo;
+	
+	/** Return String[] of d/M/yyyy dates,  from List<Blockdates> 
+	 * with LocalDate startDate and Integer period
+     *
+     * @param 
+     * @return String[] **/
+	public static String[] fdates(List<Blockdate> listBddates) {
+
+		//listBddates.forEach(b -> System.out.println("Dates blocked: " + b.getBdate() 
+		//								+ " to " + b.getEdate() ));
+		
+		//listBddates	.forEach(b -> Stream.of(listDates(b.getBdate(),b.getEdate()))
+		//			.forEach(s -> System.out.println("Stream :: " + s)));
+
+		LocalDate dummyDate = LocalDate.parse("2021-04-01");
+		LocalDate[] mergedates;
+		mergedates = dateRange(dummyDate, 0);
+		
+		for (Blockdate bd: listBddates){
+
+			LocalDate[] ld = listDates(bd.getBdate(), bd.getEdate().plusDays(1));
+			mergedates = mergeDates(mergedates, ld );
+
+			}
+
+		//Stream.of(mergedates).forEach(s -> System.out.println("mergedates :: " + s));
+		return  finalDates(mergedates);
+		
+	}
+
+	/** Return LocaDate[] of merge LocalDate[] A and LocalDate[] B
+	 * @param
+	 * @return LocalDate[] **/
 	public static LocalDate[] mergeDates(LocalDate[] listA, LocalDate[] listB) {
 		int length = listA.length + listB.length;
 		LocalDate[] mergedates = new LocalDate[length];
@@ -35,23 +58,46 @@ public class calenderBlkDates {
 		
 		return mergedates;
 	}
-
 	
+	/** return LocalDate[] array all dates from LocalDate start and end (exclude)
+	 * @param
+	 * @return LocalDate[] **/
+	public static LocalDate[] listDates(LocalDate start, LocalDate end) {
+		Integer getDays = getDays(start, end);
+		LocalDate[] listDate = dateRange(start, getDays);
+		return listDate;
+	}
+	
+	/** return Integer of days between LocalDate start date and end date 
+	 * @param
+	 * @return Integer	 **/
+	public static Integer getDays(LocalDate start, LocalDate end) {
+		Period period = Period.between(start, end);
+		Integer getDays = period.getDays();
+		return getDays;
+	}
+	
+	/* not use */
 	public static <T> Object[] mergeArray(T[] arr1, T[] arr2)   
 	{   
 		return Stream.of(arr1, arr2).flatMap(Stream::of).toArray();   
 	}   
 
+	/** return LocalDate[] with range of LocalDate start till end 
+	 * @param
+	 * @return LocalDate[]	 **/
 	public static LocalDate[] dateRange(LocalDate start, Integer daysNo) {
 		LocalDate[] dateRange = new LocalDate[daysNo];
 		for(int i=0;i<daysNo;i++) {
 			dateRange[i] = start.plusDays(i);
-			//System.out.println("=====> dateRange: " + dateRange[i] );
 		}
 		
 		return dateRange;
 	}
 	
+	/** return date format d/M/yyyy from LocalDate[] 
+	 * @param
+	 * @return String[]	 **/
 	public static String[] finalDates(LocalDate[] listdates) {
 		
 		String[] finalDates= new String[listdates.length]; // size up first
@@ -67,9 +113,9 @@ public class calenderBlkDates {
 		return finalDates;
 		
 	}
-	
 
-    //Stream of dates with 1 day difference
+    /** not use
+     *  Stream of dates with 1 day difference */
     public static List<LocalDate> getDaysInJava8(LocalDate start, int days) 
     {
         return Stream.iterate(start, date -> date.plusDays(1))
